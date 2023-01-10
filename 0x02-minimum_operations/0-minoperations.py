3ec17047b6e1fd4d9dcc7f5331679b4f3dcf071f
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-01-10 13:15:38 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,9 +13,14 @@ def minOperations(n: int) -> int:
     in the file"""
     process = 2
     op = 0
-    while (n > 1):
-        while n % 2 == 0:
-            op += process
-            n /= process
-        process += 1
-    return op
+    if (n == None or n < 0):
+        return 0
+    elif (n == 1):
+        return 1
+    elif (n > 1):
+        while (n > 1):
+            while n % process == 0:
+                op += process
+                n /= process
+            process += 1
+        return op
