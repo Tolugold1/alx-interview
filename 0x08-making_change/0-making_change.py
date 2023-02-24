7b607659f5f283428c60bce954baedab1ab9f6a2
Tolugold <toluadelekesteve@gmail.com>
2023-02-24 09:25:45 +0100

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,16 +7,16 @@ that makes up the total coin.
 def makeChange(coins, total):
     """return fewest coin"""
     if total < 1:
-      return 0
+        return 0
     needed_coin = 0
     coins.sort()
     coins.reverse()
     for i in coins:
-      if total <= 0:
-        break
-      temp = total // i
-      needed_coin += temp
-      total -= temp * i
+        if total <= 0:
+            break
+        temp = total // i
+        needed_coin += temp
+        total -= temp * i
     if total != 0:
-      return -1
+        return -1
     return needed_coin
